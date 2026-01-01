96a601c37db6fc1fbd1c571de7b69853f8371d81
yukit4mu <140926731+yukit4mu@users.noreply.github.com>
2026-01-01 07:27:32 -0500

    Add genre management, test data preparation, and tests to basic branch

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\FavoriteController;
 use App\Http\Controllers\ReviewLikeController;
 use App\Http\Controllers\GenreController;
 use App\Http\Controllers\RankingController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 // Public routes
@@ -41,6 +42,11 @@ Route::middleware('auth')->group(function () {
     // Review Like management
     Route::post('/reviews/{review}/like', [ReviewLikeController::class, 'store'])->name('likes.store');
     Route::delete('/reviews/{review}/unlike', [ReviewLikeController::class, 'destroy'])->name('likes.destroy');
+
+    // Profile management
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 // Public book show route
